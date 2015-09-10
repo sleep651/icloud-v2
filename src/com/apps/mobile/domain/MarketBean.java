f956cf533dd59762ab3030fab9ff2e778c728fdb
sleep651 <59937368@qq.com>
2015-09-10 22:45:26 +0800

    添加图片及漏提交的文件

---
+++ src/com/apps/mobile/domain/MarketBean.java
@@ -0,0 +1,117 @@
+package com.apps.mobile.domain;
+
+public class MarketBean {
+	private String serv_number;/*号码*/
+	private String user_name;/*姓名*/
+	private String brand_name;/*品牌*/
+	private String marketing_case_name;/*营销案*/
+	private String credit_name;/*信誉度等级*/
+	private String net_dur;/*网龄*/
+	private String usim_flag;/*是否USIM卡*/
+	private String arpu;/*ARPU*/
+	private String call_dur;/*通话时长*/
+	private String call_cnt;/*通话次数*/
+	private String flux_2g;/*2G流量*/
+	private String flux_3g;/*3G流量*/
+	private String flux_4g;/*4G流量*/
+	private String point_cnt;/*积分余额*/
+	private String campseg_content;/*营销用语*/
+	private String detain_info_cur;/*维系内容*/
+	
+	public String getServ_number() {
+		return serv_number;
+	}
+	public void setServ_number(String serv_number) {
+		this.serv_number = serv_number;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getBrand_name() {
+		return brand_name;
+	}
+	public void setBrand_name(String brand_name) {
+		this.brand_name = brand_name;
+	}
+	public String getMarketing_case_name() {
+		return marketing_case_name;
+	}
+	public void setMarketing_case_name(String marketing_case_name) {
+		this.marketing_case_name = marketing_case_name;
+	}
+	public String getCredit_name() {
+		return credit_name;
+	}
+	public void setCredit_name(String credit_name) {
+		this.credit_name = credit_name;
+	}
+	public String getNet_dur() {
+		return net_dur;
+	}
+	public void setNet_dur(String net_dur) {
+		this.net_dur = net_dur;
+	}
+	public String getUsim_flag() {
+		return usim_flag;
+	}
+	public void setUsim_flag(String usim_flag) {
+		this.usim_flag = usim_flag;
+	}
+	public String getArpu() {
+		return arpu;
+	}
+	public void setArpu(String arpu) {
+		this.arpu = arpu;
+	}
+	public String getCall_dur() {
+		return call_dur;
+	}
+	public void setCall_dur(String call_dur) {
+		this.call_dur = call_dur;
+	}
+	public String getCall_cnt() {
+		return call_cnt;
+	}
+	public void setCall_cnt(String call_cnt) {
+		this.call_cnt = call_cnt;
+	}
+	public String getFlux_2g() {
+		return flux_2g;
+	}
+	public void setFlux_2g(String flux_2g) {
+		this.flux_2g = flux_2g;
+	}
+	public String getFlux_3g() {
+		return flux_3g;
+	}
+	public void setFlux_3g(String flux_3g) {
+		this.flux_3g = flux_3g;
+	}
+	public String getFlux_4g() {
+		return flux_4g;
+	}
+	public void setFlux_4g(String flux_4g) {
+		this.flux_4g = flux_4g;
+	}
+	public String getPoint_cnt() {
+		return point_cnt;
+	}
+	public void setPoint_cnt(String point_cnt) {
+		this.point_cnt = point_cnt;
+	}
+	public String getCampseg_content() {
+		return campseg_content;
+	}
+	public void setCampseg_content(String campseg_content) {
+		this.campseg_content = campseg_content;
+	}
+	public String getDetain_info_cur() {
+		return detain_info_cur;
+	}
+	public void setDetain_info_cur(String detain_info_cur) {
+		this.detain_info_cur = detain_info_cur;
+	}
+}
---
+++ src/com/apps/mobile/domain/MarketGroupBean.java
@@ -0,0 +1,18 @@
+package com.apps.mobile.domain;
+
+public class MarketGroupBean {
+	private String market_id;	//营销活动ID
+	private String market_name;	//营销活动名称
+	public String getMarket_id() {
+		return market_id;
+	}
+	public void setMarket_id(String market_id) {
+		this.market_id = market_id;
+	}
+	public String getMarket_name() {
+		return market_name;
+	}
+	public void setMarket_name(String market_name) {
+		this.market_name = market_name;
+	}
+}
